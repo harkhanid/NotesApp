6329773c5154280658488c1ad30683db5644bbc3
Dharmik <harkhani.d@northeastern.edu>
2025-09-20 14:15:51 -0400

    Initialized backend

---
+++ feathernotes/src/main/java/com/dharmikharkhani/feathernotes/FeathernotesApplication.java
@@ -0,0 +1,13 @@
+package com.dharmikharkhani.feathernotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeathernotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeathernotesApplication.class, args);
+	}
+
+}
