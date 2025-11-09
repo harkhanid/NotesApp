6b3b203497ba550f1242df2ee760473857a66a14
Dharmik <harkhani.d@northeastern.edu>
2025-11-09 16:39:30 -0500

    KAN-10: Added Collaboration feature in note with dev webrtc network

--- frontend/src/components/editor/Editor.jsx
+++ frontend/src/components/editor/Editor.jsx
@@ -1,15 +1,42 @@
 import { useMemo,useEffect, useRef } from "react";
 import { useEditor, EditorContent,EditorContext } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Collaboration from '@tiptap/extension-collaboration'
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
+
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+import { WebrtcProvider } from "y-webrtc";
+
+const ydoc = new Y.Doc();
+const provider = new WebrtcProvider('my-document', ydoc);
+
+
 
 const Editor = ({initialContent, onUpdate, id}) => {
 
   const editor = useEditor({
-    onUpdate: ({editor})=>{
-      onUpdate(editor.getHTML(), id);
+    onContentError: ({disableCollaboration}) =>{
+      disableCollaboration()
     },
-    extensions: [StarterKit],
-    content: initialContent,
+    onCreate: ({editor: currentEdtor}) => {
+      provider.on('synced', (event) => {
+        if (currentEditor.isEmpty) {
+          currentEdtor.commands.setContent(initialContent);
+        }
+      });
+    },
+    extensions: [StarterKit,
+    Collaboration.configure({
+        document: ydoc,
+      }),
+      CollaborationCursor.configure({
+        provider: provider,
+        user: {
+          name: 'User ' + Math.floor(Math.random() * 100),
+          color: '#' + Math.floor(Math.random()*16777215).toString(16),
+        },     
+      })],
   });
  
   useEffect(() => {
